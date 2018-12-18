8649973a6807c05c8c5c5e2215f101956be5188d
Diego Pasquini <diego.pasquini@neppoadm.com.br>
2018-12-18 02:55:24 -0200

    disgrassa

--- ptbr/cliente.c
+++ ptbr/cliente.c
@@ -8,75 +8,141 @@
 #include <sys/ioctl.h>
 #include <ctype.h>
 #include <string.h>
- 
+#include <limits.h>
+#include <errno.h>
+
+char *stringToBinary(char *s)
+{
+    if (s == NULL) {
+        // NULL might be 0 but you cannot be sure about it
+        return NULL;
+    }
+    // get length of string without NUL
+    size_t slen = 65000;
+
+    // we cannot do that here, why?
+    // if(slen == 0){ return s;}
+
+    errno = 0;
+    // allocate "slen" (number of characters in string without NUL)
+    // times the number of bits in a "char" plus one byte for the NUL
+    // at the end of the return value
+    char *binary = malloc(slen * CHAR_BIT + 1);
+    if(binary == NULL){
+        fprintf(stderr,"malloc has failed in stringToBinary(%s): %s\n",s, strerror(errno));
+        return NULL;
+    }
+    // finally we can put our shortcut from above here
+    if (slen == 0) {
+        *binary = '\0';
+        return binary;
+    }
+    char *ptr;
+    // keep an eye on the beginning
+    char *start = binary;
+    int i,n;
+
+    // loop over the input-characters
+    ptr = s;
+    //for (ptr = s; *ptr != 'corinthians'; ptr++) {
+    for(n = 0; n < 65000; n++){
+        /* perform bitwise AND for every bit of the character */
+        // loop over the input-character bits
+        for (i = CHAR_BIT - 1; i >= 0; i--, binary++) {
+          *binary = (*ptr & 1 << i) ? '1' : '0';
+        }
+        ptr++;
+    }
+    // finalize return value
+    *binary = '\0';
+    // reset pointer to beginning
+    binary = start;
+    return binary;
+}
+
 int configuracaoCliente()
 {
-    /*Descritor do cliente*/
     int clienteSockfd;
-    /*Estrutura do Cliente*/
     struct sockaddr_in serv_addr;
-    /*Tamanho da estrutura*/
     socklen_t addrlen = sizeof (serv_addr);
- 
-    /*Socket familia TCP declarado SOCK_STREAM e criado*/
     clienteSockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (clienteSockfd < 0)
     {
         printf("Erro no Socket\n");
         exit(1);
     }
-    /*Zera a estrutura*/
     bzero((char *) & serv_addr, sizeof (serv_addr));
-    /*Seta a familia*/
     serv_addr.sin_family = AF_INET;
-    /*Define o IP nesse caso o localhost*/
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    /*Define a porta de conexao*/
     serv_addr.sin_port = htons(6881);
-   /*Faz a conexao com o servidor*/
-   if(connect(clienteSockfd,(struct sockaddr *)&serv_addr, addrlen) < 0)
-     {
-            printf("Erro no Socket\n");
+    if(connect(clienteSockfd,(struct sockaddr *)&serv_addr, addrlen) < 0)
+    {
+        printf("Erro no Socket\n");
         exit(1);
     }
-        return clienteSockfd;
+    return clienteSockfd;
 }
  
 void Cliente(int clienteSockfd)
 {
-  /*Buffer de dados a ser mandado para o servidor*/
-  char buffer_para_servidor[256];
-  printf("Digite uma mensagem para o servidor: ");
-   do {
- 
-        scanf("%s",buffer_para_servidor);
-        fflush(stdin);
-        /*Escreve para o servidor*/
- 
-        write(clienteSockfd, buffer_para_servidor, sizeof (buffer_para_servidor));
-        
-        bzero(buffer_para_servidor, sizeof(buffer_para_servidor)); 
+    char buffer_do_servidor[256];
+    printf("Making a heartbeat request...\n");
 
-        read(clienteSockfd, buffer_para_servidor, sizeof(buffer_para_servidor)); 
-        printf("From Server : %s", buffer_para_servidor); 
+    char type[12];
+    char payload_size[12];
+    char payload[256];
 
-      /*Mensagem para sair*/
-    } while (strcmp(buffer_para_servidor, "sair") != 0);
-    /**Encerra o descritor*/
+    strcpy(type,"1");
+    strcpy(payload_size,"65000");
+    strcpy(payload,"AMALICIOUSPAYLOAD");
+
+    char *blood_buffer = malloc(atoi(payload_size));
+
+    printf("Sending request type...\n");
+    write(clienteSockfd, type, sizeof(type));
+    bzero(buffer_do_servidor, sizeof (buffer_do_servidor));
+
+    read(clienteSockfd, buffer_do_servidor, sizeof (buffer_do_servidor));
+    printf("Recieved : %s\n",buffer_do_servidor);
+    bzero(buffer_do_servidor, sizeof (buffer_do_servidor));
+
+    
+    printf("Sending payload size...\n");
+    write(clienteSockfd, payload_size, sizeof(payload_size));
+
+    read(clienteSockfd, buffer_do_servidor, sizeof (buffer_do_servidor));
+    printf("Recieved : %s\n",buffer_do_servidor);
+    bzero(buffer_do_servidor, sizeof (buffer_do_servidor));
+
+    printf("Sending payload...\n");
+    write(clienteSockfd, payload, sizeof(payload));
+
+    printf("Waiting answer...\n");
+    bzero(blood_buffer, sizeof (blood_buffer));
+    read(clienteSockfd, blood_buffer, atoi(payload_size));
+    printf("Recieved (%d bytes) : %s\n",atoi(payload_size),blood_buffer);
+
+    FILE *fp;
+    fp = fopen( "heartbleed_output.txt" , "w" );
+    fwrite(blood_buffer, 1, atoi(payload_size), fp);
+    fclose(fp);
+
+    char *output_binary = stringToBinary(stdout);
+    printf("[length: %d] : %s\n",strlen(output_binary),output_binary);
+
+    fp = fopen( "heartbleed_output_binary.txt" , "w" );
+    fwrite(output_binary, 1, atoi(payload_size), fp);
+    fclose(fp);
+
     close(clienteSockfd);
 }
  
 int main()
 {
-    system("clear");
-    /*Estrutura cliente*/
     struct sockaddr_in serv_addr;
-    /*Tamanho da estrutura*/
     socklen_t addrlen = sizeof (serv_addr);
-    /*Define o descritor cliente*/
     int descritorCliente;
     descritorCliente = configuracaoCliente();
-    /*chama funcao do cliente passando o descritor*/
     Cliente(descritorCliente);
     exit(0);
 }
--- ptbr/servidor.c
+++ ptbr/servidor.c
@@ -11,66 +11,75 @@
  
 void* Servidor(void* arg)
 {
-    /*Buffer de entrada (armazena buffer do cliente)*/
     char buffer_do_cliente[256];
-    /*Cast do ponteiro*/
+    char *buffer, *pollution;
+
     int sockEntrada = *(int *) arg;
-    /*Loop "infinito"*/
-    int n;
-    
-    printf("Aguardando as mensagens.....\n");
-    for (;;)
-    {
-        /*Le o que vem do cliente*/
-        read(sockEntrada, buffer_do_cliente, sizeof (buffer_do_cliente));
-        if (strcmp(buffer_do_cliente, "sair") != 0)
-        {
-            /*Se buffer == sair cai fora*/
-            printf("\n%s",buffer_do_cliente);
-            bzero(buffer_do_cliente, sizeof (buffer_do_cliente)); 
-            n = 0;
-            // copy server message in the buffer 
-			while ((buffer_do_cliente[n++] = getchar()) != '\n') 
-				; 
-            write(sockEntrada, buffer_do_cliente, sizeof(buffer_do_cliente)); 
-        }
-        else
-             {
-                 /*Encerra o descritor*/
-                 close(sockEntrada);
-                 /*Encerra a thread*/
-                 pthread_exit((void*) 0);
-             }
+    int n, payload_size;
+    for(n = 1; n <= 1000; n++){
+        pollution = malloc(128);
+        strcpy(pollution,"GETMEMRMALICIOUS");
+        free(pollution);   
     }
+    printf("\n\n\nAguardando uma requisição de heartbeat...\n");
+
+
+    read(sockEntrada, buffer_do_cliente, sizeof (buffer_do_cliente));
+    
+    printf("Type: %s\n",buffer_do_cliente);
+
+    bzero(buffer_do_cliente, sizeof (buffer_do_cliente));
+
+    strcpy(buffer_do_cliente,"Send your payload size");
+    write(sockEntrada, buffer_do_cliente, sizeof(buffer_do_cliente));
+    bzero(buffer_do_cliente, sizeof (buffer_do_cliente));
+
+    read(sockEntrada, buffer_do_cliente, sizeof (buffer_do_cliente));
+    payload_size = atoi(buffer_do_cliente);
+    
+    buffer = malloc(sizeof(char[payload_size]));
+
+    printf("Payload size: %d\n",payload_size);
+    printf("buffer size : %d\n",sizeof(buffer));
+    bzero(buffer_do_cliente, sizeof (buffer_do_cliente));
+
+    strcpy(buffer_do_cliente,"Send your payload");
+    write(sockEntrada, buffer_do_cliente, sizeof(buffer_do_cliente));
+    bzero(buffer_do_cliente, sizeof (buffer_do_cliente));
+
+    read(sockEntrada, buffer_do_cliente, sizeof (buffer_do_cliente));
+    printf("Payload: %s\n",buffer_do_cliente);
+    memcpy(buffer,buffer_do_cliente,payload_size);
+
+    printf("Sending heartbeat response ");
+    write(sockEntrada, buffer, payload_size);
+
+/*      n = 0;
+    while ((buffer_do_cliente[n++] = getchar()) != '\n') 
+        ; */
+    close(sockEntrada);
+    pthread_exit((void*) 0);
+
 }
  
 int configuracaoServidor()
 {
-    /*Cria o descritor*/
     int sockfd;
-    /*Declaracao da estrutura*/
     struct sockaddr_in serverAddr;
-    /*Cria o socket*/
     if ((sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
     {
       printf("Erro no Socket\n");
       exit(1);
     }
-    /*Zera a estrutura*/
     memset(&serverAddr, 0, sizeof (serverAddr));
-    /*Seta a familia*/
     serverAddr.sin_family = AF_INET;
-    /*Seta os IPS (A constante INADDR_ANY e todos os ips ou qualquer ip) htonl -> conversao*/
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    /*Define a porta*/
     serverAddr.sin_port = htons(6881);
-    /*Faz a bindagem (cola, gruda, conecta seja o que for)*/
     if (bind(sockfd, (struct sockaddr *) & serverAddr, sizeof (serverAddr)) < 0)
     {
       printf("Erro no Socket\n");
       exit(1);
      }
-    /*Fica na escuta de ate 5 clientes*/
     if (listen(sockfd, 5) < 0)
     {
       printf("Erro no Socket\n");
@@ -81,35 +90,27 @@ int configuracaoServidor()
  
 int main()
 {
-    system("clear");
-    /*Declaracao da estrutura*/
     struct sockaddr_in serverAddr;
-    /*Retorna da funcao e o descritor*/
     int sockfd = configuracaoServidor();
  
-    /*Loop "infinito"*/
     while (1)
     {
         int clienteSockfd;
         struct sockaddr_in clienteAddr;
-        /*tamanho da estrutura*/
         unsigned int clntLen;
         clntLen = sizeof (clienteAddr);
-        /*declara uma thread*/
-    pthread_t thread;
-    /*Fica no aguardo da conexao do cliente*/
+        pthread_t thread;
+    
         if ((clienteSockfd = accept(sockfd, (struct sockaddr *) & clienteAddr, &clntLen)) < 0)
         {
-      printf("Erro no Socket\n");
-      exit(1);
-    }
-        /*Inicializa a thread*/
+            printf("Erro no Socket\n");
+            exit(1);
+        }
         if (pthread_create(&thread, NULL, Servidor, &clienteSockfd) != 0)
-       {
+        {
             printf("Erro na Thread\n");
             exit(1);
-       }
- 
+        }
         pthread_detach(thread);
     }
     exit(0);
